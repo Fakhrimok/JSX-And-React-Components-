c9fbc878910ae442b7880df636605c69ce35af57
fakhrimok <moknifakhri@gmail.com>
2023-04-18 10:21:51 +0100

    jsx checkpoint

---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FullName = () => {
+  return <div>Fakhri Mokni </div>;
+};
+
+export default FullName;
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfilePhoto = () => {
+  return (
+    <div>
+      <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" />
+    </div>
+  );
+};
+
+export default ProfilePhoto;
